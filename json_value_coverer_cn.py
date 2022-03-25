85b0a9df15612954f6f3660f357f94fdab4a1487
Sinbing <53322231+Sinbing@users.noreply.github.com>
2022-03-25 16:07:46 +0800

    update v0.0.2

--- json_value_coverer_cn.py
+++ json_value_coverer_cn.py
@@ -7,22 +7,22 @@ from collections import OrderedDict
 def show_usage():
     print('''
     =============== Usage ===============
-     Input [source.json] and [target.json]
-     If a key was same in both json,
-     Tool will read value in [source.json]
-     and cover it in [target.json]
-     Useful in localization files update.
+     输入[源文件.json]与[目标文件.json]。
+     如果在两个json文件中存在相同的键名；
+     会读取[源文件.json]中键名对应的键值，
+     并覆盖[目标文件.json]中该键名的键值。
+     更新汉化版本时很有用的小工具捏~
     =====================================
     ''')
 
 
 def input_json_name(tips: str):
     while True:
-        file_path = input(f'\nplese input {tips} path: ')
+        file_path = input(f'\n请输入 {tips} 的路径: ')
         if os.path.isfile(file_path) and file_path[-5:] == '.json':
             break
         else:
-            print('\n### ERROR ### \n#- Cant find json file, retry plese. -#')
+            print('\n### 出BUG啰 ### \n#- 你这输入的是json文件吗，我咋找不到捏，检查下文件路径再试试看呗。 -#')
     return file_path
 
 
@@ -44,19 +44,19 @@ def update_json(source_json: dict, target_json: dict):
 
 
 def output_file(json_dict: dict):
-    file_name: str = input('\nplese input [output file name]: ') + '.json'
+    file_name: str = input('\n请输入 [输出用的文件名]: ') + '.json'
     try:
         with open(file_name, 'w', encoding='UTF-8') as file:
             file.write(json.dumps(json_dict, indent=4, ensure_ascii=False))
-        print(f'\n### SUCCESS ### \n#- Cover {cover_count} value done. -#')
+        print(f'\n### 成功啰 ### \n#- 爷替换了足足 {cover_count} 条键值诶！ -#')
     except:
-        print(f'### ERROR ### \n#- Can\'t write output file. -#')
+        print(f'### 出BUG啰 ### \n#- 没办法创建输出文件，你再好好检查一下吧！ -#')
 
 
 
 if __name__ == '__main__':
     show_usage()
-    source_file, target_file = input_json_name('[source.json]'), input_json_name('[target.json]')
+    source_file, target_file = input_json_name('[源文件.json]'), input_json_name('[目标文件.json]')
     source_dict, target_dict = read_json(source_file), read_json(target_file)
     new_json, cover_count = update_json(source_dict, target_dict)
     output_file(new_json)
